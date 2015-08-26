06993e07a43989494ce39ca0621ee22addb23a84
m.kopaczynski <m.kopaczynski@Kopa-PC.centrala.kaczmarski.pl>
2015-08-26 15:43:12 +0200

    first app

---
+++ FindTeam/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindTeam.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      ViewBag.Message = "Find your team.";
+
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Website designed to find best team.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
---
+++ FindTeam/Models/AccountModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace FindTeam.Models
+{
+  public class UsersContext : DbContext
+  {
+    public UsersContext()
+      : base("DefaultConnection")
+    {
+    }
+
+    public DbSet<UserProfile> UserProfiles { get; set; }
+  }
+
+  [Table("UserProfile")]
+  public class UserProfile
+  {
+    [Key]
+    [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+  }
+
+  public class RegisterExternalLoginModel
+  {
+    [Required]
+    [Display(Name = "User name")]
+    public string UserName { get; set; }
+
+    public string ExternalLoginData { get; set; }
+  }
+  
+ 
+}
